bf8e8532e5eeb9c108b861e8278658ef5efdf5aa
abbyd <abbydiaz300@gmail.com>
2023-08-14 19:51:28 -0700

    made time lines editable with a click

--- Develop/script.js
+++ Develop/script.js
@@ -2,4 +2,28 @@
 var now = dayjs().format("MMMM DD, YYYY");
 $('#currentDay').html(now);
 
+var tasks = {}
+
+
+// time block was clicked and wants to be edited
+$(".time-txt").on("click", function() {
+    var text = $(this)
+    .text()
+    .trim();
+    // replacing ul with new text area
+    var textInput = $("<textarea>")
+    .addClass("textarea col-sm-9 p-3")
+    .val(text);
+    // auto focus new element 
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+};
+
+
+
 
