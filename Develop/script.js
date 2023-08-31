43144fbd139950dab14b89d7fb59c36abb0c46a0
abbyd <abbydiaz300@gmail.com>
2023-08-30 19:30:33 -0700

    got tasks key and value pair to populate

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,10 @@
 var now = dayjs().format("MMMM DD, YYYY");
 $('#currentDay').html(now);
 
-var tasks = {}
-
+var tasks = [];
 
 // time block was clicked and wants to be edited
-$(".time-txt").on("click", function() {
+$(".time-txt").on("click",  function() {
     var text = $(this)
     .text()
     .trim();
@@ -19,10 +18,84 @@ $(".time-txt").on("click", function() {
     textInput.trigger("focus");
 });
 
-
 var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+
+
+// editable field was unfocused 
+$(".time-txt").on("blur", "textarea", function() {
+    // get textarea current value/text
+    var text = $(this)
+    .val()
+    .trim();
+
+    // get status type and postion in list
+    var status = $(this)
+    .closest(".hour")
+    .attr("id")
+    // chaining with attr, which returns the id, so list- followed by the category
+    .replace("num-", "");
+    // onced chained .replace will remove list- from the txt which will give the category name
+
+    // get tasks position in teh list of other li elements 
+    var index = $(this)
+    .closest(".time-txt")
+    .index();
+
+    // update tasks in array and re-scave to local storage
+    tasks[status][index].text = text;
+
+    saveTasks();
+
+    
+}); 
+
+
+// save button clicked 
+$(".saveBtn").on("click",  function() {
+    //get task text from the textare in the same row
+    var taskText = $(this)
+    .closest(".hour")
+    .siblings(".time-txt")
+    .find("textarea")
+    .val();
+
+    var taskTime = $(this)
+    .closest(".hour")
+    .attr("id")
+    //.replace("num-", "");
+
+    saveTasks();
+
+
+    console.log('save button clicked');
+
+
+    if (taskText && taskTime) {
+        // save in tasks array
+        tasks.push ({
+            text: taskText,
+            time: taskTime
+        });
+        saveTasks();
+    }
+});
+
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!tasks) {
+        tasks = {
+            text: [],
+            time: []
+        };
+    }
+ 
+};
+
+loadTasks();
 
 
 
